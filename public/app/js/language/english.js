2de8736a0c4acfb5c2cb03fd48cc6e60ad0f6ebd
Adrien Dulong <adrien.dulong@gmail.com>
2014-02-11 16:30:07 +0100

    Corrections fichiers langues page Event
    Corrections fichiers langues page Event

--- public/app/js/language/english.js
+++ public/app/js/language/english.js
@@ -3,7 +3,7 @@
 *************************************/
 
 
-var photoview_waiting_title = " Please wait a few seconds ...";
+var photoview_waiting_title = " Please wait";
 var photoview_waiting_subtitle = "  We are loading your content ...";
 
 var photoview_comment_sending = " We are sending the comment ...";
@@ -17,18 +17,18 @@ var photoview_likethis = " like this ";
 *************************************/
 
 //modal connexion facebook
-var message_connection_obligatoire = " <b> Oops ! <\/b> To access to this event you have to log in thought Facebook and let Woovent manage your facebook event ";
-var message_connection_obligatoire2 = " To access to the photos of the Facebook event <span class=\"event-name-login\"></span> , you have to be connected with your Facebook account ";
+var message_connection_obligatoire = " <b> Oops ! <\/b> To access to this event you have to log in to Facebook and let Woovent manage your Facebook events ";
+var message_connection_obligatoire2 = " To access to the photos of the Facebook event <span class=\"event-name-login\"></span>, you have to be connected to your Facebook account ";
 var message_connection_obligatoire_modal_title = "  Welcome on Woovent !";
 var message_connection_obligatoire_modal_button = "Discover <span class=\"login-count-photo\"></span> photos";
 
 //modal share facebook
-var message_share_modal_alert = " Oops ! To tell your friends (only with posting link on the event wall), you should authorize Woovent App ! <br /> No worries, we won't post anything without your permission !";
+var message_share_modal_alert = " Oops ! To let your friends know (only with a link to the photos on the event wall), you have to authorize the Woovent App ! <br /> No worries, we won't post anything without your permission !";
 var message_share_modal_content = " If you want, we can give the link only to the attendees, in posting into the event wall a link to the photos you have just added ?";
-var message_share_modal_title = " Tell your friends ! ";
-var message_share_modal_button = " Tell my friends ";
+var message_share_modal_title = " Let the guests know! ";
+var message_share_modal_button = "Let the guests know";
 
-var event_button_info = "See events information";
+var event_button_info = "See the event's information";
 var event_share_text = "Share the link of the event with your Friends";
 
 //modal ajout des photos
--- public/app/js/language/french.js
+++ public/app/js/language/french.js
@@ -3,10 +3,10 @@
 *************************************/
 
 
-var photoview_waiting_title = " Chargement en cours ...";
-var photoview_waiting_subtitle = " Chargement en cours ...";
+var photoview_waiting_title = " Veuiller patienter";
+var photoview_waiting_subtitle = " Nous chargeons votre contenu...";
 
-var photoview_comment_sending = " Envoi du commentaire en cours ...";
+var photoview_comment_sending = " Envoie du commentaire en cours ...";
 
 var photoview_nolike = " Aucun like pour le moment ";
 var photoview_and = " et ";
@@ -17,8 +17,8 @@ var photoview_likethis = " aime ";
 *************************************/
 
 //modal connexion facebook
-var message_connection_obligatoire = " <b> Oops ! <\/b> Pour pouvoir acceder à notre service vous devez vous connecter via Facebook et autoriser l'application Woovent à gérer vos évènements ";
-var message_connection_obligatoire2 = " Pour acceder aux photos de l'évènement <span class=\"event-name-login\"></span> facebook vous devez être connecté à l'aide de votre compte Facebook";
+var message_connection_obligatoire = " <b> Oops ! <\/b> Pour pouvoir acceder à notre service vous devez vous connecter via Facebook et autoriser l'application Woovent à gérer vos évènements. ";
+var message_connection_obligatoire2 = " Pour acceder aux photos de l'évènement <span class=\"event-name-login\"></span> Facebook vous devez être connecté à l'aide de votre compte Facebook.";
 var message_connection_obligatoire_modal_title = " Bienvenue sur Woovent !";
 var message_connection_obligatoire_modal_button = "Voir les <span class=\"login-count-photo\"></span> photos !";
 
@@ -27,9 +27,9 @@ var message_ajout_photo_attente = "Merci de patienter encore un tout petit peu :
 var message_ajout_photo_modal_title = " Ajout en cours !";
 
 //modal share facebook
-var message_share_modal_alert = " Oops ! Pour avertir vos amis (en déposant uniquement un lien sur le mur de l'évènement vous devez autoriser l'application Woovent ! <br /> Pas d'inquietude, nous ne posterons rien sans votre autorisation !";
+var message_share_modal_alert = " Oops ! Pour avertir vos amis (en déposant uniquement un lien sur le mur de l'évènement) vous devez autoriser l'application Woovent ! <br /> Pas d'inquietude, nous ne posterons rien sans votre autorisation !";
 var message_share_modal_content = " Si vous le souhaitez, nous pouvons indiquer uniquement aux invités, en postant sur le mur de l'évènement le lien vers les photos que vous venez de rajouter ? ";
-var message_share_modal_title = " Prevenez vos invités ! ";
+var message_share_modal_title = " Prevenez les invités ! ";
 var message_share_modal_button = " Prévenir les invités ";
 
 //html
